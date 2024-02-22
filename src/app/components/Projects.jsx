571b94e7ae0b31edb1c4984fc76f3b4a9551fadb
Ketan Kumavat <ketan.kumavat1984@gmail.com>
2024-02-22 18:35:36 +0530

    working on work section

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -7,8 +7,142 @@ import {
   createHttpLink,
   gql,
 } from "@apollo/client";
-
 import { setContext } from "@apollo/client/link/context";
+import Image from "next/image";
+
+function useStarredRepositories() {
+  const [starredRepositories, setStarredRepositories] = useState([]);
+
+  useEffect(() => {
+    const fetchStarredRepositories = async () => {
+      const httpLink = createHttpLink({
+        uri: "https://api.github.com/graphql",
+      });
+
+      const authLink = setContext((_, { headers }) => {
+        return {
+          headers: {
+            ...headers,
+            authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN}`,
+          },
+        };
+      });
+
+      const client = new ApolloClient({
+        link: authLink.concat(httpLink),
+        cache: new InMemoryCache(),
+      });
+
+      const { data } = await client.query({
+        query: gql`
+          {
+            user(login: "KetanKumavat") {
+              starredRepositories(first: 10) {
+                edges {
+                  node {
+                    ... on Repository {
+                      id
+                      name
+                      url
+                      description
+                      createdAt
+                      primaryLanguage {
+                        name
+                      }
+                      homepageUrl
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `,
+      });
+
+      if (data?.user?.starredRepositories?.edges) {
+        const starredRepositoriesData = data.user.starredRepositories.edges.map(
+          ({ node }) => node
+        );
+        const repositoriesWithReadme = await Promise.all(
+          starredRepositoriesData.map(async (repo) => {
+            const readmeResponse = await fetch(
+              `https://raw.githubusercontent.com/KetanKumavat/${repo.name}/main/README.md`
+            );
+            const readmeText = await readmeResponse.text();
+            return { ...repo, readmeText };
+          })
+        );
+        setStarredRepositories(repositoriesWithReadme);
+      }
+    };
+
+    fetchStarredRepositories();
+  }, []);
+
+  return starredRepositories;
+}
+
+function extractImageUrls(text) {
+  const regex = /<img.*?src="(.*?)".*?>/g;
+  const matches = [];
+  let match;
+  while ((match = regex.exec(text))) {
+    matches.push(match[1]);
+  }
+  return matches;
+}
+
+function Projects() {
+  const starredRepositories = useStarredRepositories();
+
+  return (
+    <div className="w-full h-screen overflow-y-scroll grid place-items-center">
+      {starredRepositories.map((repo) => {
+        const imageUrls = extractImageUrls(repo.readmeText || "");
+        return (
+          <div
+            key={repo.id}
+            className="bg-zinc-950 p-6 my-4 w-2/5 aspect-square rounded-lg shadow-lg">
+            <h2 className="text-white font-bold text-xl">{repo.name}</h2>
+            {repo.description && (
+              <p className="text-gray-300 mt-2">{repo.description}</p>
+            )}
+            <div className="flex flex-wrap mt-4">
+              {imageUrls.map((imageUrl, index) => (
+                <Image
+                  key={index}
+                  src={imageUrl}
+                  alt={`Image ${index}`}
+                  className="w-32 h-32 object-cover mr-2 mb-2 rounded-lg"
+                />
+              ))}
+            </div>
+            <div className="mt-4">
+              <a
+                href={repo.url}
+                className="text-blue-500 hover:underline"
+                target="_blank"
+                rel="noopener noreferrer">
+                View Github Repository
+              </a>
+              {repo.homepageUrl && (
+                <a
+                  href={repo.homepageUrl}
+                  className="ml-4 text-blue-500 hover:underline"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  View Project
+                </a>
+              )}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Projects;
 
 // function usePinnedrepos() {
 //   const [pinnedrepos, setPinnedrepos] = useState([]);
@@ -69,69 +203,6 @@ import { setContext } from "@apollo/client/link/context";
 //   return pinnedrepos;
 // }
 
-function useStarredRepositories() {
-  const [starredRepositories, setStarredRepositories] = useState([]);
-
-  useEffect(() => {
-    const fetchStarredRepositories = async () => {
-      const httpLink = createHttpLink({
-        uri: "https://api.github.com/graphql",
-      });
-
-      const authLink = setContext((_, { headers }) => {
-        return {
-          headers: {
-            ...headers,
-            authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN}`,
-          },
-        };
-      });
-
-      const client = new ApolloClient({
-        link: authLink.concat(httpLink),
-        cache: new InMemoryCache(),
-      });
-
-      const { data } = await client.query({
-        query: gql`
-          {
-            user(login: "KetanKumavat") {
-              starredRepositories(first: 10) {
-                edges {
-                  node {
-                    ... on Repository {
-                      id
-                      name
-                      url
-                      description
-                      createdAt
-                      primaryLanguage {
-                        name
-                      }
-                      homepageUrl
-                    }
-                  }
-                }
-              }
-            }
-          }
-        `,
-      });
-
-      if (data?.user?.starredRepositories?.edges) {
-        const starredRepositoriesData = data.user.starredRepositories.edges.map(
-          ({ node }) => node
-        );
-        setStarredRepositories(starredRepositoriesData);
-      }
-    };
-
-    fetchStarredRepositories();
-  }, []);
-
-  return starredRepositories;
-}
-
 // for pinned repo
 
 // function Projects() {
@@ -175,43 +246,3 @@ function useStarredRepositories() {
 // }
 
 //for starred repo
-
-function Projects() {
-  const starredRepositories = useStarredRepositories();
-   return (
-     <div className="w-full h-screen flex flex-col justify-center items-center">
-       {starredRepositories.map((repo) => (
-         <div
-           key={repo.id}
-           className="bg-gray-800 p-6 my-4 w-96 rounded-lg shadow-lg">
-           <h2 className="text-white font-bold text-xl">{repo.name}</h2>
-           {repo.description && (
-             <p className="text-gray-300 mt-2">{repo.description}</p>
-           )}
-           <div className="mt-4">
-             <a
-               href={repo.url}
-               className="text-blue-500 hover:underline"
-               target="_blank"
-               rel="noopener noreferrer">
-               View Github Repository
-             </a>
-             {repo.homepageUrl && (
-               <a
-                 href={repo.homepageUrl}
-                 className="ml-4 text-blue-500 hover:underline"
-                 target="_blank"
-                 rel="noopener noreferrer">
-                 View Deployment
-               </a>
-             )}
-           </div>
-         </div>
-       ))}
-     </div>
-   );
-}
-
-
-
-export default Projects;
