e8cc2a765b958f9d644118322d1ba44195ba1cc3
Ketan Kumavat <ketan.kumavat1984@gmail.com>
2024-02-24 22:43:26 +0530

    about us

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -1,9 +1,21 @@
 "use client";
 import React from "react";
 import { TextGenerateEffect } from "./ui/text-generate-effect.tsx";
+import Image from 'next/image';
+import Ketan from "../../../public/ketan1.jpg";
 
-const words = `Hi, I'm Ketan Kumavat, a 2nd-year engineering student passionate about Full Stack MERN Development. I excel in frontend technologies like React, Next.js, and Tailwind CSS. My team and I clinched 1st prize in the DevQuest Hackathon by IIT Jodhpur, fueling my ambition to become a pro in full stack development.`;
+const words = `Hi, I'm Ketan Kumavat, a 2nd-year engineering student passionate about Full Stack MERN Development. My team and I clinched 1st prize in the DevQuest Hackathon by IIT Jodhpur, fueling my ambition to become a pro in full stack development.`;
 
 export default function TextGenerateEffectDemo() {
-  return <TextGenerateEffect words={words} />;
+  return (
+    <div className="w-full mt-[25vh] flex h-screen">
+      <div className="w-full mt-16 ml-24 text-sm flex">
+        <TextGenerateEffect words={words} className="w-1/2 text-left"/>
+        <div className="flex justify-center items-center">
+         <Image src={Ketan} alt="Ketan" className="scale-50 flex mb-96 mr-20" />
+      </div>
+      </div>
+      
+    </div>
+  );
 }
--- src/app/components/ui/text-generate-effect.tsx
+++ src/app/components/ui/text-generate-effect.tsx
@@ -32,8 +32,7 @@ export const TextGenerateEffect = ({
           return (
             <motion.span
               key={word + idx}
-              className="dark:text-white text-black opacity-0"
-            >
+              className="dark:text-white text-black opacity-0">
               {word}{" "}
             </motion.span>
           );
@@ -43,9 +42,9 @@ export const TextGenerateEffect = ({
   };
 
   return (
-    <div className={cn("font-bold", className)}>
+    <div className={cn("font-semibold", className)}>
       <div className="mt-4">
-        <div className=" dark:text-white text-black text-2xl leading-snug tracking-wide">
+        <div className=" dark:text-white text-black text-2xl leading-snug ">
           {renderWords()}
         </div>
       </div>
